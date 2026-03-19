3ce0aceba6cde6d4053830ba73f0ad7ccdbb94ca
Artur Opala <250927+arturopala@users.noreply.github.com>
2026-03-19 09:40:37 +0000

    seasonal dependencies update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.11.0"
+  private val bootstrapVersion = "10.7.0"
+  private val hmrcMongoVersion = "2.12.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
@@ -16,7 +16,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion    % Test,
     "org.playframework"      %% "play-test"               % PlayVersion.current % Test,
     "org.scalatest"          %% "scalatest"               % "3.2.19"            % Test,
-    "org.scalamock"          %% "scalamock"               % "7.5.2"             % Test,
+    "org.scalamock"          %% "scalamock"               % "7.5.5"             % Test,
     "org.scalatestplus"      %% "scalacheck-1-18"         % "3.2.19.0"          % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2"             % Test,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.8"            % Test,
